466ec74dab52b6ff21114078d111d622909f0123
Jacques Terblanche <jacques.terblanche@dotdigital.com>
2020-11-21 12:07:03 +0200

    Remove scaffolded code

--- TaxCalculator/TaxCalculator.Core/Context/TaxManContext.cs
+++ TaxCalculator/TaxCalculator.Core/Context/TaxManContext.cs
@@ -30,74 +30,7 @@ namespace TaxCalculator.Core.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<PostalCode>(entity =>
-            //{
-            //    entity.HasKey(e => new { e.PostalCode1, e.TaxTypeId })
-            //        .HasName("PK_postcode");
 
-            //    entity.ToTable("PostalCode");
-
-            //    entity.Property(e => e.PostalCode1)
-            //        .HasMaxLength(10)
-            //        .HasColumnName("PostalCode");
-            //});
-
-            //modelBuilder.Entity<TaxBand>(entity =>
-            //{
-            //    entity.HasKey(e => e.TaxRateCode)
-            //        .HasName("PK__TaxBands__58C0182AAF938850");
-
-            //    entity.Property(e => e.TaxRateCode).HasMaxLength(3);
-
-            //    entity.Property(e => e.CreatedDate).HasColumnType("datetime");
-
-            //    entity.Property(e => e.LowerLimit).HasColumnType("decimal(9, 3)");
-
-            //    entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
-
-            //    entity.Property(e => e.UpperLimit).HasColumnType("decimal(9, 3)");
-            //});
-
-            //modelBuilder.Entity<TaxCalculation>(entity =>
-            //{
-            //    entity.HasNoKey();
-
-            //    entity.Property(e => e.CalculatedTaxValue).HasColumnType("decimal(10, 3)");
-
-            //    entity.Property(e => e.DateCreated).HasColumnType("datetime");
-
-            //    entity.Property(e => e.IncomeValue).HasColumnType("decimal(9, 2)");
-
-            //    entity.Property(e => e.PostalCode)
-            //        .IsRequired()
-            //        .HasMaxLength(10);
-            //});
-
-            //modelBuilder.Entity<TaxRate>(entity =>
-            //{
-            //    entity.HasNoKey();
-
-            //    entity.Property(e => e.CreatedDate).HasColumnType("datetime");
-
-            //    entity.Property(e => e.RateValue).HasColumnType("decimal(8, 3)");
-
-            //    entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
-            //});
-
-            //modelBuilder.Entity<TaxType>(entity =>
-            //{
-            //    entity.HasNoKey();
-
-            //    entity.ToTable("TaxType");
-
-            //    entity.Property(e => e.TaxTypeDescription)
-            //        .IsRequired()
-            //        .HasMaxLength(255);
-            //});
-
-            //OnModelCreatingPartial(modelBuilder);
         }
-
-        //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
